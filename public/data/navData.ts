a067969bea2510c1211eda55752768acfaaa42ec
Pranjal birla <birlapranjal460@gmail.com>
2024-07-24 18:24:43 +0530

    added link in navbar

--- public/data/navData.ts
+++ public/data/navData.ts
@@ -8,15 +8,15 @@ export const navData = [
     path: "/#vision",
   },
   {
-    title: "Rewards",
-    path: "/#Rewards",
-  },
-  {
     title: "Timeline",
     path: "/#timeline",
   },
   {
     title: "FAQs",
     path: "/#faqs",
+  },
+  {
+    title: "Results",
+    path: "/results",
   },
 ];
